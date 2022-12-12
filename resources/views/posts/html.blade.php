506c415f6203ef3cd735aa0979b0b84b8eb38f68
andy sarbini <kangandy09@gmail.com>
2022-12-12 14:45:16 +0700

    insert data dan sweet alert
    https://www.youtube.com/watch?v=qFbXDwzx6Us&list=PLXb304dovEsFqWHQFcEEttCFtO6aDTIMJ&index=8&ab_channel=CybernautsID
    minus:
    1. data belum tampil di grid
    2. slug belum berfungsi
    3. belum edit dan hapus

--- resources/views/posts/html.blade.php
+++ resources/views/posts/html.blade.php
@@ -8,7 +8,7 @@
             <!-- /.card-header -->
             <div class="row ml-3 mt-3">
                 <div class="col-4">
-                    <a href="posts/create" class="btn btn-success"><i class="fa-fa-plus"> Add</i></a>
+                    <a href="posts/create" class="btn btn-success"><i class="fa fa-plus"> Add</i></a>
                 </div>
             </div>
             <div class="card-body">
@@ -22,8 +22,17 @@
                   <th>Action</th>
                 </tr>
                 </thead>
-                <tbody>
-                
+                <tbody class="text-center">
+                  <tr>
+                    <td>1</td>
+                    <td>Laravel Blog Tutorial</td>
+                    <td>Laravel</td>
+                    <td>maret 2009</td>
+                    <td>
+                      <button class="btn btn-md btn-warning" type="button">Edit</button>
+                      <button class="btn btn-md btn-danger" type="button">Delete</button>
+                    </td>
+                  </tr>
                 </tbody>
                 <tfoot>
                 <tr>
